8e8f3f41726745472a880c8e91bb8066c42686ca
Arsha-Tehrani <142565377+Arsha-Tehrani@users.noreply.github.com>
2026-06-17 16:19:04 -0400

    Add: reconstruct-flight-path - submission-2

---
+++ Data Structures & Algorithms/reconstruct-flight-path/submission-2.py
@@ -0,0 +1,38 @@
+class Solution:
+    def findItinerary(self, tickets: List[List[str]]) -> List[str]:
+        adj = defaultdict(list)        
+        for i in tickets:
+            adj[i[0]].append(i[1])
+            
+        # FIXED 1: Added sorting to satisfy the lexicographical order rule
+        for key in adj:
+            adj[key].sort()
+
+        cur = ["JFK"]
+        res = []
+
+        def dfs(place):            
+            if len(cur) == len(tickets) + 1:
+                res.append(cur.copy())
+                # FIXED 2: Removed `cur = ["JFK"]`. Backtracking (cur.pop) handles cleanup.
+                # FIXED 3: Changed `return` to `return True` so the caller knows to stop.
+                return True
+
+            
+            for dest in range(len(adj[place])):
+                temp = adj[place][dest]
+                if temp == "skip":
+                    continue
+
+                adj[place][dest] = "skip"
+                cur.append(temp)
+                if dfs(temp):
+                    return True
+                adj[place][dest] = temp
+                cur.pop()
+
+            return False
+
+        dfs("JFK")
+        # FIXED 4: Returned the actual path array instead of printing a nested list
+        return res[0]
